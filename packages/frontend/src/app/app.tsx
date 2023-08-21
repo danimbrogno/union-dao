8d167fd74bc2a1847b3f241e8d8394b12e1cff9c
zach-is-my-name <zach.greco@gmail.com>
2023-08-21 00:31:09 -0400

    WIP: App.tsx Routes

--- packages/frontend/src/app/app.tsx
+++ packages/frontend/src/app/app.tsx
@@ -3,7 +3,7 @@ import {ConnectWallet, RegisterUnion, MemberDash, AdminDash, RegisterMember } fr
 import { useState, useEffect} from 'react'; 
 import { Route, Routes } from 'react-router-dom';
 import { useAccount, useContractReads } from 'wagmi'
-import UnionABI from '../abis/UnionABI.json'
+import {unionFacetABI}  from '../abis/generated'
 import {useConfig} from '../Config'
 const StyledApp = styled.div`
 display: flex;
@@ -17,30 +17,41 @@ text-align: center;
 export function App() {
   const {isConnected} = useAccount();
   const [entity, setEntity] = useState<string | null>("Steve");
+  const [isMember, setIsMember] = useState<boolean | null >(null);
+  const [isAdmin, setIsAdmin] = useState<boolean | null >(null);
   const {addresses} = useConfig() 
   const [unionContract] = useState(({
     address: `0x${addresses.diamond.slice(0,3)}`,
-    abi: UnionABI,
+    abi: unionFacetABI,
   }));
 
   useEffect(() => {
     if (isConnected && entity) {
-    const {data, isError, isLoading} =  useContractReads({
+    const {data, isError, isLoading, isSuccess} =  useContractReads({
       contracts: [ 
-        {address: `0x${addresses.diamond.slice(0,3)}`, abi: [UnionABI], functionName: 'isMember'},
-        {address: `0x${addresses.diamond.slice(0,3)}`, abi: [UnionABI], functionName: 'isAdmin'}
-
+        {address: `0x${addresses.diamond.slice(0,3)}`, abi: unionFacetABI, functionName: 'isMember'},
+        {address: `0x${addresses.diamond.slice(0,3)}`, abi: unionFacetABI, functionName: 'isAdmin'}
       ],
     })
+    if (isSuccess && data?.length) {
+      if (data[0].result) {
+        setIsMember(true) 
+      } else {setIsMember(false)}
+    }
+    if (isSuccess && data?.length) {
+      if (data[1].result) {
+        setIsAdmin(true) 
+      } else {setIsAdmin(false)}
+    }
+
   }
   }, [isConnected])
 
   return (
       <StyledApp>
       <Routes>
-      {isConnected?  <Route path="/member-dash" element={<MemberDash/>}/>: 
+      {isConnected ?  <Route path="/member-dash" element={<MemberDash/>}/>: 
       <Route path="/" element={<ConnectWallet/>}  /> }
-      {/* Render "/" conditionally */ }
 
       <Route path="/register-member" element={<RegisterMember/>}/>
 
--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -1,9 +1,30 @@
-const { composePlugins, withNx } = require('@nx/webpack');
+const { composePlugins, withNx, ProvidePlugin } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
+const webpack = require('webpack');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve('crypto-browserify'),
+    'crypto-browserify': require.resolve('crypto-browserify'),
+    path: require.resolve('path-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    fs: require.resolve('browserify-fs'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer/'),
+  };
+
+  config.plugins.push(
+    // fix "process is not defined" error:
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    })
+  );
   return config;
 });
